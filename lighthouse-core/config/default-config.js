4ff2abcb47aee3d084cdfa4b75a509875259a56a
Saavan Nanavati <saavan.nanavati@utexas.edu>
2020-07-14 18:12:07 -0500

    Remove bloated terminology

--- lighthouse-core/config/default-config.js
+++ lighthouse-core/config/default-config.js
@@ -242,6 +242,7 @@ const defaultConfig = {
     'non-composited-animations',
     'unsized-images',
     'bloated-libraries',
+    'large-javascript-dependencies',
     'manual/pwa-cross-browser',
     'manual/pwa-page-transitions',
     'manual/pwa-each-page-has-url',
@@ -474,6 +475,7 @@ const defaultConfig = {
         {id: 'non-composited-animations', weight: 0, group: 'diagnostics'},
         {id: 'unsized-images', weight: 0, group: 'diagnostics'},
         {id: 'bloated-libraries', weight: 0, group: 'diagnostics'},
+        {id: 'large-javascript-dependencies', weight: 0, group: 'diagnostics'},
         // Audits past this point don't belong to a group and will not be shown automatically
         {id: 'network-requests', weight: 0},
         {id: 'network-rtt', weight: 0},
