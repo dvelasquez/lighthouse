0cc96f32229acfe9c327a3598bb5520b242cd671
Saavan Nanavati <saavan.nanavati@utexas.edu>
2020-07-31 01:55:58 -0500

    Make diagnostic audit

--- lighthouse-core/config/default-config.js
+++ lighthouse-core/config/default-config.js
@@ -244,6 +244,7 @@ const defaultConfig = {
     'bloated-libraries',
     'large-javascript-dependencies',
     'large-javascript-libraries',
+    'large-javascript-libraries',
     'manual/pwa-cross-browser',
     'manual/pwa-page-transitions',
     'manual/pwa-each-page-has-url',
@@ -479,6 +480,7 @@ const defaultConfig = {
         {id: 'unsized-images', weight: 0, group: 'diagnostics'},
         {id: 'bloated-libraries', weight: 0, group: 'diagnostics'},
         {id: 'large-javascript-dependencies', weight: 0, group: 'diagnostics'},
+        {id: 'large-javascript-libraries', weight: 0, group: 'diagnostics'},
         // Audits past this point don't belong to a group and will not be shown automatically
         {id: 'network-requests', weight: 0},
         {id: 'network-rtt', weight: 0},
